0f5699462135a87740ae36d1451325e455ec5e60
sreeav6 <139438620+sreeav6@users.noreply.github.com>
2023-07-15 15:07:02 +0530

    Create add-sub.py

---
+++ src/add-sub.py
@@ -0,0 +1,12 @@
+# app.py
+# This is a test commit
+def add(a, b):
+    return a + b
+def sub(a,b)
+    return a - b
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
+def test_sub():
+    assert sub(3, 2) == 1
+    assert add(2, -2) == 0
